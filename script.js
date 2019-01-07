4b90aee33628f31be4c88ab59e627d29681448c6
Ray Kugler <raymond.kugler@gmail.com>
2019-01-07 09:18:57 -0800

    first qb

---
+++ script.js
@@ -0,0 +1,11 @@
+
+ test = () => {
+    let comp = document.getElementById('complete').value;
+    let attempts = document.getElementById('attempt').value;
+    
+    let test = parseInt(comp) + parseInt(attempts);     
+     console.log(test);
+ }
+
+
+//  document.getElementById("demo").innerHTML = x
