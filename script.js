36a3f01815ce5ea898103688876c3ccda8652b1a
Ray Kugler <raymond.kugler@gmail.com>
2019-01-14 15:00:56 -0800

    red green

--- script.js
+++ script.js
@@ -1,48 +1,5 @@
-let qbs = [
-    {name: 'Drew Brees', rating: 115.7, name_column: 0,name_row: 0 ,rating_column:1},
-    {name: 'Patrick Mahomes', rating: 113.8, name_column: 0,name_row:1 ,rating_column:1 },
-    {name: 'Russell Wilson', rating: 110.9, name_column: 0,name_row: 2,rating_column: 1},
-    {name: 'Matt Ryan', rating: 108.1, name_column: 0,name_row: 3,rating_column: 1},
-    {name: 'Philip Rivers', rating: 105.5, name_column: 0,name_row: 4,rating_column:1 },
-    {name: 'Deshaun Watson', rating: 103.1, name_column: 0,name_row: 5,rating_column:1 },
-    {name: 'Carson Wentz', rating: 102.2, name_column: 0,name_row: 6,rating_column: 1},
-    {name: 'Jared Goff', rating: 101.1, name_column: 0,name_row:7 ,rating_column: 1},
-    {name: 'Ryan Fitzpatrick',  rating: 100.4, name_column:0 ,name_row:8 ,rating_column: 1},
-    {name: 'Kirk Cousins',  rating: 99.7, name_column: 0,name_row: 9,rating_column: 1},
 
-    {name: 'Andrew Luck',  rating: 98.7, name_column: 2,name_row: 0,rating_column: 3},
-    {name: 'Tom Brady',  rating: 97.7, name_column: 2,name_row: 1,rating_column: 3},
-    {name: 'Aaron Rodgers',  rating: 97.6, name_column: 2,name_row:2 ,rating_column:3 },
-    {name: 'Dak Prescott',  rating: 96.9, name_column: 2,name_row: 3,rating_column: 3},
-    {name: 'Ben Roethlisberger',  rating: 96.5, name_column: 2,name_row: 4,rating_column: 3},
-    {name: 'Nick Foles',  rating: 96.0, name_column: 2,name_row: 5,rating_column: 3},
-    {name: 'Mitchell Trubisky',  rating: 95.4, name_column:2 ,name_row:6 ,rating_column:3 },
-    {name: 'Cam Newton',  rating: 94.2, name_column: 2,name_row: 7,rating_column: 3},
-    {name: 'Derek Carr',  rating: 93.9, name_column: 2,name_row: 8,rating_column: 3},
-    {name: 'Baker Mayfield',  rating: 93.7 , name_column: 2,name_row: 9,rating_column: 3},
-    
-    {name: 'Ryan Tannehill',  rating: 92.7, name_column: 4,name_row: 0,rating_column: 5},
-    {name: 'Eli Manning',  rating: 92.4, name_column: 4,name_row: 1,rating_column: 5},
-    {name: 'Marcus Mariota',  rating: 92.3, name_column: 4,name_row:2 ,rating_column: 5},
-    {name: 'Nick Mullens',  rating: 90.8, name_column: 4,name_row: 3,rating_column: 5 },
-    {name: 'Jameis Winston',  rating: 90.2, name_column: 4,name_row:4 ,rating_column: 5},
-    {name: 'Matthew Stafford',  rating: 89.9, name_column: 4,name_row:5 ,rating_column: 5},
-    {name: 'Andy Dalton',  rating: 89.6, name_column: 4,name_row: 6,rating_column: 5},
-    {name: 'Brock Osweiler',  rating: 86.0, name_column: 4,name_row: 7,rating_column: 5},
-    {name: 'Alex Smith',  rating: 85.7, name_column: 4,name_row: 8,rating_column: 5},
-    {name: 'Lamar Jackson',  rating: 84.5, name_column: 4,name_row: 9,rating_column: 5},
-    
-    {name: 'Joe Flacco',  rating: 84.2, name_column: 6,name_row: 0,rating_column: 7},
-    {name: 'Jeff Driskel',  rating: 82.2, name_column: 6,name_row:1 ,rating_column: 7},
-    {name: 'C.J Beathard',  rating: 81.8, name_column: 6,name_row: 2,rating_column: 7},
-    {name: 'Case Keenum',  rating: 81.2, name_column: 6,name_row: 3,rating_column: 7},
-    {name: 'Blake Bortles',  rating: 79.8, name_column: 6,name_row: 4,rating_column: 7},
-    {name: 'Sam Darnold',  rating: 77.6, name_column:6 ,name_row: 5,rating_column: 7},
-    {name: 'Cody Kessler',  rating: 77.4, name_column:6 ,name_row: 6,rating_column: 7},
-    {name: 'Blaine Gabbert',  rating: 74.9, name_column: 6,name_row:7 ,rating_column: 7},
-    {name: 'Josh Allen',  rating: 67.9, name_column: 6,name_row: 8,rating_column: 7},
-    {name: 'Josh Rosen',  rating: 66.7, name_column: 6,name_row: 9,rating_column: 7},
-];
+
  rating = () => {
     let array = []
     let comp = parseInt(document.getElementById('complete').value);
@@ -87,7 +44,79 @@ let qbs = [
     let rating = ((a + b + c + d) /6 ) * 100;
     let fa = rating.toFixed(2);
     // let chart =document.getElementById("name_grab");
-    // for(j=0; j< 10; j++){
+    let chart = document.getElementById("name_grab");
+    let ax = chart.rows[0].cells[1];
+        let bx = chart.rows[1].cells[1];
+        let cx = chart.rows[2].cells[1];
+        let dx = chart.rows[3].cells[1];
+        let ex = chart.rows[4].cells[1];
+        let fx = chart.rows[5].cells[1];
+        let gx = chart.rows[6].cells[1];
+        let hx = chart.rows[7].cells[1];
+        let ix = chart.rows[8].cells[1];
+        let jx = chart.rows[9].cells[1];
+
+        let az = chart.rows[0].cells[3];
+        let bz = chart.rows[1].cells[3];
+        let cz = chart.rows[2].cells[3];
+        let dz = chart.rows[3].cells[3];
+        let ez = chart.rows[4].cells[3];
+        let fz = chart.rows[5].cells[3];
+        let gz = chart.rows[6].cells[3];
+        let hz = chart.rows[7].cells[3];
+        let iz = chart.rows[8].cells[3];
+        let jz = chart.rows[9].cells[3];
+
+        let ay = chart.rows[0].cells[5];
+        let by = chart.rows[1].cells[5];
+        let cy = chart.rows[2].cells[5];
+        let dy = chart.rows[3].cells[5];
+        let ey = chart.rows[4].cells[5];
+        let fy = chart.rows[5].cells[5];
+        let gy = chart.rows[6].cells[5];
+        let hy = chart.rows[7].cells[5];
+        let iy = chart.rows[8].cells[5];
+        let jy = chart.rows[9].cells[5];
+
+        let aw = chart.rows[0].cells[7];
+        let bw = chart.rows[1].cells[7];
+        let cw = chart.rows[2].cells[7];
+        let dw = chart.rows[3].cells[7];
+        let ew = chart.rows[4].cells[7];
+        let fw = chart.rows[5].cells[7];
+        let gw = chart.rows[6].cells[7];
+        let hw = chart.rows[7].cells[7];
+        let iw = chart.rows[8].cells[7];
+        let jw = chart.rows[9].cells[7];
+    
+    let qbs = [ax, bx, cx, dx, ex, fx ,gx, hx,ix , jx, az, bz, cz, dz, ez, fz, gz, hz, iz, jz, ay, by,cy,dy,ey, fy, gy,hy,iy,jy, aw,bw,cw,dw,ew,fw,gw,hw,iw,jw];
+    for(j= 0; j< 40; j++){
+        let current = qbs[j].innerHTML;
+        let grab = '';
+        if(current < rating){
+            qbs[j].classList.remove('green');
+            qbs[j].classList.add('red');
+            grab = 'less than';
+        }
+        else{
+            qbs[j].classList.remove('red');
+            qbs[j].classList.add('green');
+            grab = 'greater than';
+        }
+        qbs[j].classList.add('red');
+        console.log(j + ' : ' + current + ' is ' + grab + ' ' + rating);
+        // if (fa < current){
+        // qbs[j].classList.remove('green');   
+        // qbs[j].classList.add('red');
+        // console.log('less than ' +  current);
+        // }
+        // else{
+        //     qbs[j].classList.remove('red');   
+        //     qbs[j].classList.add('green');
+        //     console.log('more than ' +  current);
+
+        // }
+    }
     //     for(p=0; p<8; p++){
     //         let selected = chart.rows[j].cells[p];
 
@@ -136,3 +165,49 @@ let qbs = [
 
 // const euros = [29.76, 41.85, 46.5];
 // const sum = euros.reduce((total, amount) => total + amount); 
+
+// let qbs = [
+//     {name: 'Drew Brees', rating: 115.7, name_column: 0,name_row: 0 ,rating_column:1},
+//     {name: 'Patrick Mahomes', rating: 113.8, name_column: 0,name_row:1 ,rating_column:1 },
+//     {name: 'Russell Wilson', rating: 110.9, name_column: 0,name_row: 2,rating_column: 1},
+//     {name: 'Matt Ryan', rating: 108.1, name_column: 0,name_row: 3,rating_column: 1},
+//     {name: 'Philip Rivers', rating: 105.5, name_column: 0,name_row: 4,rating_column:1 },
+//     {name: 'Deshaun Watson', rating: 103.1, name_column: 0,name_row: 5,rating_column:1 },
+//     {name: 'Carson Wentz', rating: 102.2, name_column: 0,name_row: 6,rating_column: 1},
+//     {name: 'Jared Goff', rating: 101.1, name_column: 0,name_row:7 ,rating_column: 1},
+//     {name: 'Ryan Fitzpatrick',  rating: 100.4, name_column:0 ,name_row:8 ,rating_column: 1},
+//     {name: 'Kirk Cousins',  rating: 99.7, name_column: 0,name_row: 9,rating_column: 1},
+
+//     {name: 'Andrew Luck',  rating: 98.7, name_column: 2,name_row: 0,rating_column: 3},
+//     {name: 'Tom Brady',  rating: 97.7, name_column: 2,name_row: 1,rating_column: 3},
+//     {name: 'Aaron Rodgers',  rating: 97.6, name_column: 2,name_row:2 ,rating_column:3 },
+//     {name: 'Dak Prescott',  rating: 96.9, name_column: 2,name_row: 3,rating_column: 3},
+//     {name: 'Ben Roethlisberger',  rating: 96.5, name_column: 2,name_row: 4,rating_column: 3},
+//     {name: 'Nick Foles',  rating: 96.0, name_column: 2,name_row: 5,rating_column: 3},
+//     {name: 'Mitchell Trubisky',  rating: 95.4, name_column:2 ,name_row:6 ,rating_column:3 },
+//     {name: 'Cam Newton',  rating: 94.2, name_column: 2,name_row: 7,rating_column: 3},
+//     {name: 'Derek Carr',  rating: 93.9, name_column: 2,name_row: 8,rating_column: 3},
+//     {name: 'Baker Mayfield',  rating: 93.7 , name_column: 2,name_row: 9,rating_column: 3},
+    
+//     {name: 'Ryan Tannehill',  rating: 92.7, name_column: 4,name_row: 0,rating_column: 5},
+//     {name: 'Eli Manning',  rating: 92.4, name_column: 4,name_row: 1,rating_column: 5},
+//     {name: 'Marcus Mariota',  rating: 92.3, name_column: 4,name_row:2 ,rating_column: 5},
+//     {name: 'Nick Mullens',  rating: 90.8, name_column: 4,name_row: 3,rating_column: 5 },
+//     {name: 'Jameis Winston',  rating: 90.2, name_column: 4,name_row:4 ,rating_column: 5},
+//     {name: 'Matthew Stafford',  rating: 89.9, name_column: 4,name_row:5 ,rating_column: 5},
+//     {name: 'Andy Dalton',  rating: 89.6, name_column: 4,name_row: 6,rating_column: 5},
+//     {name: 'Brock Osweiler',  rating: 86.0, name_column: 4,name_row: 7,rating_column: 5},
+//     {name: 'Alex Smith',  rating: 85.7, name_column: 4,name_row: 8,rating_column: 5},
+//     {name: 'Lamar Jackson',  rating: 84.5, name_column: 4,name_row: 9,rating_column: 5},
+    
+//     {name: 'Joe Flacco',  rating: 84.2, name_column: 6,name_row: 0,rating_column: 7},
+//     {name: 'Jeff Driskel',  rating: 82.2, name_column: 6,name_row:1 ,rating_column: 7},
+//     {name: 'C.J Beathard',  rating: 81.8, name_column: 6,name_row: 2,rating_column: 7},
+//     {name: 'Case Keenum',  rating: 81.2, name_column: 6,name_row: 3,rating_column: 7},
+//     {name: 'Blake Bortles',  rating: 79.8, name_column: 6,name_row: 4,rating_column: 7},
+//     {name: 'Sam Darnold',  rating: 77.6, name_column:6 ,name_row: 5,rating_column: 7},
+//     {name: 'Cody Kessler',  rating: 77.4, name_column:6 ,name_row: 6,rating_column: 7},
+//     {name: 'Blaine Gabbert',  rating: 74.9, name_column: 6,name_row:7 ,rating_column: 7},
+//     {name: 'Josh Allen',  rating: 67.9, name_column: 6,name_row: 8,rating_column: 7},
+//     {name: 'Josh Rosen',  rating: 66.7, name_column: 6,name_row: 9,rating_column: 7},
+// ];
